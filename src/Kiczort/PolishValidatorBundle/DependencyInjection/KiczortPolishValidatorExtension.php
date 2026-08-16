38ab5c0da84ae8e9b5dc17913556f6af76c6d2dc
Michal Mleczko <c-mmleczko@xpansiv.com>
2026-08-16 21:14:17 +0200

    Use non-deprecated DependencyInjection Extension base class
    Symfony\Component\HttpKernel\DependencyInjection\Extension is
    deprecated since symfony/http-kernel 8.1 in favor of
    Symfony\Component\DependencyInjection\Extension\Extension, which is
    available since Symfony 6.4. The old class was triggering a
    deprecation notice that phpunit-bridge turns into a CI failure on the
    PHP 8.5 + Symfony 8 job.

--- src/Kiczort/PolishValidatorBundle/DependencyInjection/KiczortPolishValidatorExtension.php
+++ src/Kiczort/PolishValidatorBundle/DependencyInjection/KiczortPolishValidatorExtension.php
@@ -13,7 +13,7 @@ namespace Kiczort\PolishValidatorBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
